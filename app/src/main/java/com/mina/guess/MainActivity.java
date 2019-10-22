b4b4457e595afaf652ba8e77abec8ee3064fe58d
mina93612 <mina93612@gmail.com>
2019-10-22 20:27:12 +0800

    2th commit

--- app/src/main/java/com/mina/guess/MainActivity.java
+++ app/src/main/java/com/mina/guess/MainActivity.java
@@ -8,40 +8,47 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    float number = (float)Math.random();
-    number *=10;
-    int number2 = (int)number;
+     String TAG =MainActivity.class.getSimpleName();
+     private EditText number;
+     int secret = new Random().nextInt(10)+1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
-
         setSupportActionBar(toolbar);
-
+        number=findViewById(R.id.inform);
+        Log.d(TAG,"Secret:"+secret);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view) {
-                int i = 0;
-                setResult(i);
-                int guess = ;
-                if(guess>number2)
-                    result.set;
-
-
-
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+    }
+    public void Guess (View view){
+        TextView inform = findViewById(R.id.inform);
+        int num = Integer.parseInt(number.getText().toString());
+        if (num>secret){
+            inform.setText("Bigger!!");
+        } else if (num < secret) {
+            inform.setText("Smaller!!");
+        }else
+            inform.setText("Bingo!!!");
     }
 
     @Override
